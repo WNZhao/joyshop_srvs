5be92c86e26f542d5d5770230e9cf206755dc007
walker <walker@walkerdeMacBook-Pro.local>
2025-07-25 18:30:06 +0700

    更新nacos连接及配置

--- user_srv/config/config.go
+++ user_srv/config/config.go
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-05-08 17:58:32
- * @LastEditors: Will nanan_zhao@163.com
- * @LastEditTime: 2025-05-12 16:58:25
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-07-25 18:10:44
  * @FilePath: /joyshop_srvs/user_srv/config/config.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
